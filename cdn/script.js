ec3deac36f59b075848df74cc5029b577cd8d1f1
Nu Imran <nizam1uddin2002@gmail.com>
2023-04-04 09:37:41 +0600

    Create script.js

---
+++ cdn/script.js
@@ -0,0 +1,36 @@
+const form = document.querySelector('form');
+
+form.addEventListener('submit', async (e) => {
+  e.preventDefault();
+
+  const websiteURL = document.querySelector('input[type="text"]').value;
+
+  // Check domain age
+  const response1 = await fetch(`https://api.domainsdb.info/v1/domains/search?domain=${websiteURL}`);
+  const data1 = await response1.json();
+  const domainAge = new Date(data1.domains[0].create_date);
+  console.log(`Domain age: ${new Date().getFullYear() - domainAge.getFullYear()} years`);
+
+  // Check hosting information
+  const response2 = await fetch(`https://api.hackertarget.com/hostsearch/?q=${websiteURL}`);
+  const data2 = await response2.text();
+  console.log(`Hosting information: ${data2}`);
+
+  // Check website speed
+  const response3 = await fetch(`https://www.googleapis.com/pagespeedonline/v5/runPagespeed?url=${websiteURL}`);
+  const data3 = await response3.json();
+  const speedScore = data3.lighthouseResult.categories.performance.score;
+  console.log(`Website speed score: ${speedScore}`);
+
+  // Check website framework
+  const script = document.createElement('script');
+  script.src = `https://builtwith.com/builtwith.js?d=${websiteURL}`;
+  document.body.appendChild(script);
+
+  setTimeout(() => {
+    const technologies = BuiltWith && BuiltWith.allTechnology;
+    console.log(`Website framework: ${technologies}`);
+  }, 2000);
+
+ 
+});
